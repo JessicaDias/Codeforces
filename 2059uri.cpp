19e8dbb1a34b79e8f77ec6670ff574ed9fca1380
JessicaDias <jc.jooy@gmail.com>
2017-02-17 23:57:21 -0200

    Ímpar, Par ou Roubo

---
+++ 2059uri.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int p, j1, j2, r, a;
+	cin >> p >> j1 >> j2 >> r >> a;
+
+	if (r == 1 && a == 1){
+		cout << "Jogador 2 ganha!" << endl;
+	}
+	else {
+		if (r == 0 && a == 1){
+			cout << "Jogador 1 ganha!" << endl;
+		}
+                          else{
+                            if (r == 1){
+                                cout << "Jogador 1 ganha!" << endl;
+                            }
+                            else {
+				if ((j1 + j2)%2 == 0){ //Soma é par
+					if (p == 1){
+						cout << "Jogador 1 ganha!" << endl;
+					}
+					else {
+						cout << "Jogador 2 ganha!" << endl;
+					}
+				}
+				else { //Soma é ímpar
+					if (p == 0){
+						cout << "Jogador 1 ganha!" << endl;
+					}
+					else {
+						cout << "Jogador 2 ganha!" << endl;
+					}
+				}
+			}
+		}
+    }
+	return 0;
+}
+
