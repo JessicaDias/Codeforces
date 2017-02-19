fd270d4bb1af7e65cb2b0fa7d243fabb7a0f986e
JessicaDias <jc.jooy@gmail.com>
2017-02-19 17:35:31 -0300

    Andando no tempo

---
+++ 2235uri.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b ,c, r = 0;
+
+    cin >> a >> b >> c;
+
+    if (a == b || a == c || c == b){
+        r = 1;
+    } else {
+        if (a+b == c || a+c == b || c+b == a){
+            r = 1;
+        }
+    }
+
+    if (r == 1){
+        cout << "S" << endl;
+    } else {
+        cout << "N" << endl;
+    }
+    return 0;
+}
